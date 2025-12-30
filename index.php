8c9cfe7a098c634b69e82920ff88699965022f6d
Elakkiya vetrivel <elakkiyavetrivel13@gmail.com>
2025-12-30 11:29:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>student management system</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+    <nav>
+        <label class="logo">W-school</label>
+
+        <ul>
+            <li><a href="">home</a></li>
+            <li><a href="">contact</a></li>
+            <li><a href="">admission</a></li>
+            <li><a href="">login</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
