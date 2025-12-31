3d1bb335f386de1741ee8dd47d6f4ac782afb509
Elakkiya vetrivel <elakkiyavetrivel13@gmail.com>
2025-12-31 16:30:02 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,25 +1,74 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>student management system</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<title>Student Management System</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
-
 <body>
-    <nav>
-        <label class="logo">W-school</label>
 
-        <ul>
-            <li><a href="">home</a></li>
-            <li><a href="">contact</a></li>
-            <li><a href="">admission</a></li>
-            <li><a href="">login</a></li>
-        </ul>
-    </nav>
-</body>
+	<nav>
+		<label class="logo">W-School</label>
 
-</html>
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Contact</a></li>
+			<li><a href="">Admission</a></li>
+			<li><a href="login.php" class="btn btn-success">Login</a></li>
+		</ul>
+	</nav>
+
+
+	<div class="section1">
+		
+		<label class="img_text">We Teach Students With Care</label>
+		<img class="main_img" src="school.png">
+	</div>
+
+
+	<div class="container">
+
+		<div class="row">
+
+			<div class="col-md-4">
+
+				<img class="welcome_img" src="school2.jpg">
+				
+			</div>
+
+			<div class="col-md-8">
+
+				<h1>Welcome to W-School</h1>
+
+				<p>MEMS has been committed to global learning long before it became an indispensable feature of contemporary education. Established in 1997, we proudly stand as the 1st English medium school in Bangladesh to adopt both Pearson Edexcel and Cambridge curriculum (in O and A levels), drawing together students in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated.MEMS has been committed to global learning long before it became an indispensable feature of contemporary education. Established in 1997, we proudly stand as the 1st English medium school in Bangladesh to adopt both Pearson Edexcel and Cambridge curriculum (in O and A levels), drawing together students in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated.</p>
+				
+			</div>
+			
+
+		</div>
+		
+
+	</div>
+
+    <div>
+        <h2 class="our_teachers">our teachers</h2>
+      <div class="container">
+        <div class="col-md-3">
+            <div class="teacher">
+
+            
+            </div>
+        </div>
+      </div> 
+    </div>
+
+
